dfdf6b628bf3796704ed73acb4bcd811f5ab627b
LEE LUNA <seojilee@student.42seoul.kr>
2025-04-10 13:32:42 +0900

    3.5: the twelve days of christmas - change meta datas to constants

--- projects/3.5-branches/the-twelve-days-of-christmas/src/main.rs
+++ projects/3.5-branches/the-twelve-days-of-christmas/src/main.rs
@@ -1,52 +1,51 @@
 const SSIZE: usize = 12;
 
+const NUMBERS: [&str; SSIZE] = [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "11th",
+    "12th",
+];
+
+const LYRICS: [&str; SSIZE] = [
+    "12 drummers drumming",
+    "Eleven pipers piping",
+    "Ten lords a-leaping",
+    "Nine ladies dancing",
+    "Eight maids a-milking",
+    "Seven swans a-swimming",
+    "Six geese a-laying",
+    "Five golden rings (five golden rings)",
+    "Four calling birds",
+    "Three French hens",
+    "Two turtle-doves",
+    "And a partridge in a pear tree",
+];
+
 fn main() {
-    let numbers: [&str; SSIZE] = [
-        "first",
-        "second",
-        "third",
-        "fourth",
-        "fifth",
-        "sixth",
-        "seventh",
-        "eighth",
-        "ninth",
-        "tenth",
-        "11th",
-        "12th",
-    ];
-
-    let lyrics: [&str; SSIZE] = [
-        "12 drummers drumming",
-        "Eleven pipers piping",
-        "Ten lords a-leaping",
-        "Nine ladies dancing",
-        "Eight maids a-milking",
-        "Seven swans a-swimming",
-        "Six geese a-laying",
-        "Five golden rings (five golden rings)",
-        "Four calling birds",
-        "Three French hens",
-        "Two turtle-doves",
-        "And a partridge in a pear tree",
-    ];
-
-    print_lyrics(0, &numbers, &lyrics);
-    for i in 1..SSIZE {
-        print_lyrics(i, &numbers, &lyrics);
+    for i in 0..SSIZE {
+        print_ith_lyrics(i);
     }
 }
 
-fn print_lyrics(i: usize, numbers: & [&str; SSIZE], lyrics: & [&str; SSIZE]) {
-    println!("On the {} day of Christmas", numbers[i]);
+fn print_ith_lyrics(i: usize) {
+    println!("On the {} day of Christmas", NUMBERS[i]);
     println!("My true love sent to me");
 
     if i == 0 {
-        println!("A {}", &lyrics[SSIZE - 1]["And a ".len()..]);
+        println!("A {}", &LYRICS[SSIZE - 1]["And a ".len()..]);
     } else {
 
         for j in (0..i+1).rev() {
-            println!("{}", lyrics[SSIZE - j - 1]);
+            println!("{}", LYRICS[SSIZE - j - 1]);
         }
     }
 
